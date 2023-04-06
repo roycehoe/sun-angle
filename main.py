8a55bb2f3a7fa0c76b2b859cc0721bdde328562e
roycehoe <royceinbox@hotmail.com>
2023-04-06 18:31:10 +0800

    feat: Create function to calculate sun position

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from dotenv import dotenv_values
+from suncalc import get_position
 
 env_values = dotenv_values()
 WINDOW_DIRECTION = env_values.get("WINDOW_DIRECTION")
-LATITUDE = env_values.get("LATITUDE")
-LONGITUDE = env_values.get("LONGITUDE")
+LATITUDE = float(env_values.get("LATITUDE"))
+LONGITUDE = float(env_values.get("LONGITUDE"))
 ELEVATION = env_values.get("ELEVATION")
+
+
+@dataclass
+class SunPosition:
+    azimuth: float
+    altitude: float
+
+
+def get_sun_position(lon: float, lat: float, date=datetime.now()) -> SunPosition:
+    date = datetime.now()
+    return SunPosition(**get_position(date, lon, lat))
+
+
+print(get_sun_position(LONGITUDE, LATITUDE))
