e8728596ebf3043dfba8e71d3465e91fc3081c7a
roycehoe <royceinbox@hotmail.com>
2023-04-06 17:44:05 +0800

    feat: Add latitude, longitude, elevation and window direction to main file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from dataclasses import dataclass
+
 from dotenv import dotenv_values
 
 env_values = dotenv_values()
-POSTAL_CODE = env_values.get("POSTAL_CODE")
+WINDOW_DIRECTION = env_values.get("WINDOW_DIRECTION")
+LATITUDE = env_values.get("LATITUDE")
+LONGITUDE = env_values.get("LONGITUDE")
+ELEVATION = env_values.get("ELEVATION")
